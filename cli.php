b686823a9c01068fc34109b03c2e1ef22d35a2da
ms <schmiddim@gmx.at>
2015-09-29 01:34:23 +0200

    format output

--- cli.php
+++ cli.php
@@ -7,13 +7,33 @@
 
 require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 
+
 //wohnzimmer 58117
 //büro 58114
 $thingSpeakChannels = array(58117, 58114);
 
+
+/**
+ * found here http://stackoverflow.com/a/11871948
+ * @param $input
+ * @param $pad_length
+ * @param string $pad_string
+ * @param int $pad_type
+ * @return string
+ */
+function mb_str_pad($input, $pad_length, $pad_string = ' ', $pad_type = STR_PAD_RIGHT) {
+	mb_internal_encoding('utf-8'); // @important
+	$diff = strlen($input) - mb_strlen($input);
+	return str_pad($input, $pad_length + $diff, $pad_string, $pad_type);
+}
+
 $client = new Zend\Http\Client();
 
+$maxLenChannelName = 0;
+$maxLenChannelValue = 0;
 
+$channelNames = array();
+$channelValues = array();
 foreach ($thingSpeakChannels as $channelId) {
 	$url = sprintf('http://api.thingspeak.com/channels/%d/feed.json', $channelId);
 	$client->setUri($url)->setMethod(\Zend\Http\Request::METHOD_GET);
@@ -22,7 +42,22 @@ foreach ($thingSpeakChannels as $channelId) {
 	$lastRecord = end($responseObject->feeds);
 	$d = new \DateTime($lastRecord->created_at);
 	$d->setTimezone(new \DateTimeZone(date_default_timezone_get()));
-	echo $responseObject->channel->name . ' ' . $d->format('H:m:s') . ' ' . $lastRecord->field1 . ' °C' . PHP_EOL;
 
+	if (mb_strlen($responseObject->channel->name) > $maxLenChannelName) {
+		$maxLenChannelName = mb_strlen($responseObject->channel->name);
+	}
+	if (mb_strlen($lastRecord->field1) > $maxLenChannelValue) {
+		$maxLenChannelValue = mb_strlen($lastRecord->field1);
+	}
+
+	$channelNames[] = $responseObject->channel->name;
+	$channelValues[] = $lastRecord->field1;
 
 }
+
+$str = '';
+foreach ($channelNames as $index => $channelName) {
+	$str .= mb_str_pad($channelName, $maxLenChannelName + 3, ' ', STR_PAD_RIGHT);
+	$str .= $channelValues[$index] . ' °C' . PHP_EOL;
+}
+echo $str;

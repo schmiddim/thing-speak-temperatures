139dbd9a1080edc3ea03fddbc3e4b37ad4cff742
ms <schmiddim@gmx.at>
2015-09-29 02:24:32 +0200

    display interval

--- cli.php
+++ cli.php
@@ -31,17 +31,30 @@ $client = new Zend\Http\Client();
 
 $maxLenChannelName = 0;
 $maxLenChannelValue = 0;
+$maxLenDateIntervalInSeconds = 0;
 
 $channelNames = array();
 $channelValues = array();
+$dateValues = array();
+$dateIntervalsInSeconds = array();
 foreach ($thingSpeakChannels as $channelId) {
 	$url = sprintf('http://api.thingspeak.com/channels/%d/feed.json', $channelId);
 	$client->setUri($url)->setMethod(\Zend\Http\Request::METHOD_GET);
 	$response = $client->send();
+
 	$responseObject = \Zend\Json\Json::decode($response->getBody());
 	$lastRecord = end($responseObject->feeds);
-	$d = new \DateTime($lastRecord->created_at);
-	$d->setTimezone(new \DateTimeZone(date_default_timezone_get()));
+
+	$date = new \DateTime($lastRecord->created_at);
+	$date->setTimezone(new \DateTimeZone(date_default_timezone_get()));
+	$now = new \DateTime();
+	//@see http://php.net/manual/de/dateinterval.format.php
+	$interval = $now->diff($date);
+	$dateIntervalsInSeconds[] = $interval->s;
+	$dateValues[] = $date->format('Y-m-d H:i:s');
+
+	$channelNames[] = $responseObject->channel->name;
+	$channelValues[] = $lastRecord->field1;
 
 	if (mb_strlen($responseObject->channel->name) > $maxLenChannelName) {
 		$maxLenChannelName = mb_strlen($responseObject->channel->name);
@@ -49,14 +62,16 @@ foreach ($thingSpeakChannels as $channelId) {
 	if (mb_strlen($lastRecord->field1) > $maxLenChannelValue) {
 		$maxLenChannelValue = mb_strlen($lastRecord->field1);
 	}
+	if (mb_strlen($interval->s) > $maxLenDateIntervalInSeconds) {
+		$maxLenDateIntervalInSeconds = mb_strlen($interval->s);
+	}
 
-	$channelNames[] = $responseObject->channel->name;
-	$channelValues[] = $lastRecord->field1;
 
 }
 
 $str = '';
 foreach ($channelNames as $index => $channelName) {
+	$str .= mb_str_pad($dateIntervalsInSeconds [$index] . ' s ago', $maxLenDateIntervalInSeconds + 3, ' ', STR_PAD_RIGHT);
 	$str .= mb_str_pad($channelName, $maxLenChannelName + 3, ' ', STR_PAD_RIGHT);
 	$str .= $channelValues[$index] . ' °C' . PHP_EOL;
 }
